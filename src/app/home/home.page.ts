af07c2e5840674d8944c3a564c269d66bd44e035
Bart van der Wal <bartvanderwal@gmail.com>
2019-09-05 13:10:19 +0200

    Start of calculator; plus working.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  output: string;
 
+  accumulator: number;
+
+  lastOperator: string;
+
+  hasStartedTyping: boolean;
+
+  constructor() {
+  }
+
+  ionViewDidEnter() {
+    this.hasStartedTyping = false;
+    this.output = '0';
+  }
+
+  pushEquals() {
+    switch (this.lastOperator) {
+      case '+':
+        this.output = '' + (this.accumulator + parseInt(this.output, 10));
+        break;
+      case '-':
+        // TODO.
+        break;
+      default:
+        alert('deze operator bestaat (nog) niet');
+    }
+    this.hasStartedTyping = false;
+  }
+
+  pushOperator(event) {
+    // Na een operator kiezen kan de gebruiker een nieuw getal gaan intikken.
+    this.hasStartedTyping = false;
+
+    const currentOperator = event.target.innerHTML;
+    this.accumulator = parseInt(this.output, 10);
+    this.lastOperator = currentOperator;
+  }
+
+  push(event) {
+    const currentDigit = event.target.innerHTML;
+    if (this.hasStartedTyping) {
+      this.output += currentDigit;
+    } else {
+      this.output = currentDigit;
+      this.hasStartedTyping = true;
+    }
+  }
+
+  clear() {
+    this.output = '0';
+  }
 }
